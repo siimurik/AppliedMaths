2f4dcbdcee9500c840f0df8f123adeb5d50637d4
siimurik <82040815+siimurik@users.noreply.github.com>
2022-12-28 13:48:51 +0200

    Update prob_A4.m

--- NumMeth/prob_A4.m
+++ NumMeth/prob_A4.m
@@ -11,7 +11,7 @@ h = 2/n;
 x = 0:h:2;
 A = zeros(n+1,n+1);
 y = zeros(1, n+1);
-A(1,1) = 2/h + h/2*(4-x(1));
+A(1,1) = 1/h + h/2*(4-x(1));
 A(1,2) = -1/h;
 y(1) = h/2*(x(1)+5) - 1;
 for i = 2:n
@@ -21,7 +21,7 @@ for i = 2:n
     y(i)     = h*(x(i)+5);
 end
 A(n+1,n)   = -1/h;
-A(n+1,n+1) = 2/h + h/2*(4-x(n+1));
+A(n+1,n+1) = 1/h + h/2*(4-x(n+1));
 y(n+1)     = h/2*(x(n+1)+5) - 1;
 %%
 u = A\y';
@@ -29,8 +29,3 @@ plot(x,u)
 xlabel('x')
 ylabel('u')
 grid on
-%%%
-for i=1:n+1
-    ue(i)= 0.5*x(i) + 4;
-end
-error=max(max(abs(u-ue)))
